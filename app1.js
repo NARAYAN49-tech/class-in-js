6b2c54704a7ed3f19fdb339b9a76b176363c181a
Narayan Rajkumar Suryawanshi <narayanrsuryawanshi@gmail.com>
2024-11-25 13:03:50 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,27 @@
+const stud1 = {
+
+    name : "narayan",
+    age : 19, 
+    marks  : 40,
+    getMarks: function() {
+        return this.marks;
+    },
+};
+
+const stud2 = {
+    name : "john",
+    age : 20,
+    marks : 45,
+    getMarks: function() {
+        return this.marks;
+    },
+};
+
+const stud3 = {
+    name : "johny",
+    age : 21,
+    marks : 50,
+    getMarks: function() {
+        return this.marks;
+    },
+};
